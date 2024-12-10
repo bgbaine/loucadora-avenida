88be140e65783f49381b11f8939c9d1ac14c8fd3
Bernardo <bgbaine@gmail.com>
2024-12-09 23:00:32 -0300

    feat: implement checarFilme method

--- src/models/Filme.ts
+++ src/models/Filme.ts
@@ -21,36 +21,46 @@ export class Filme {
     this._imdb = imdb;
     this._ano = ano;
     this._pais = pais;
-    Filme.filmes.push(this);
   }
 
   public get titulo(): string {
     return this._titulo;
   }
 
-  /* TODO: dependendo da arquitetura, criar metodo adicionarFilme()
-    se for caso: passar para csv ANTES 
-    public adicionarFilme(): void  {}*/
+  public static checarFilme(imdb: string): boolean {
+    return Filme.filmes.some((filme) => filme._imdb === imdb);
+  }
 
-    public static adicionarFilme(filme: Filme): void {
-      Filme.filmes.push(filme);
-      console.log(`Filme ${filme.titulo} adicionado com sucesso.`);
-    }
+  /* TODO: dependendo da arquitetura, criar metodo adicionarFilme() */
+  public static adicionarFilme(filme: Filme): void {
+    Filme.filmes.push(filme);
+    console.log(`Filme ${filme.titulo} adicionado com sucesso.`);
+  }
 
   // TODO: implementar metodo atualizarFilme(): passar para csv ANTES
-  public atualizarFilme(titulo?: string, autor?: string, ano?: number, pais?: string): void {
-    if (titulo) this._titulo = titulo;
-    if (autor) this._autor = autor;
-    if (ano) this._ano = ano;
-    if (pais) this._pais = pais;
-  
-    console.log(`Filme ${this.titulo} atualizado com sucesso.`);
-    // TODO: salvar alterações no CSV
+  public static atualizarFilme(
+    imdb: string,
+    titulo?: string,
+    autor?: string,
+    ano?: number,
+    pais?: string
+  ): void {
+    const filme = Filme.filmes.find((filme) => filme._imdb === imdb);
+    if (filme) {
+      if (titulo) filme._titulo = titulo;
+      if (autor) filme._autor = autor;
+      if (ano) filme._ano = ano;
+      if (pais) filme._pais = pais;
+
+      console.log(`Filme ${filme.titulo} atualizado com sucesso.`);
+    } else {
+      console.log("Filme não encontrado.");
+    }
   }
 
   // TODO: implementar metodo removerFilme(): passar para csv ANTES
   public static removerFilme(imdb: string): void {
-    const index = Filme.filmes.findIndex(filme => filme._imdb === imdb);
+    const index = Filme.filmes.findIndex((filme) => filme._imdb === imdb);
     if (index !== -1) {
       const filmeRemovido = Filme.filmes.splice(index, 1);
       console.log(`Filme ${filmeRemovido[0].titulo} removido com sucesso.`);
@@ -61,36 +71,17 @@ export class Filme {
   }
 
   // TODO: passar para csv
- /*  public static listarFilmes(): Filme[] {
-    console.log("Listando filmes...");
-    return Filme.filmes;
-  } */
-
-  /* public static listarFilmes(): void {
+  public static listarFilmes(): void {
     if (Filme.filmes.length === 0) {
       console.log("Nenhum filme cadastrado.");
     } else {
       console.log("Listando filmes:");
-      Filme.filmes.forEach(filme => {
-        console.log(`Título: ${filme.titulo}`);
-        console.log(`Autor: ${filme._autor}`);
-        console.log(`Ano: ${filme._ano}`);
-        console.log(`País: ${filme._pais}`);
-        console.log(`IMDB: ${filme._imdb}`);
-        console.log("----");
+      Filme.filmes.forEach((filme) => {
+        console.log(
+          `Título: ${filme.titulo}, Autor: ${filme._autor}, Ano: ${filme._ano}, País: ${filme._pais}, IMDb: ${filme._imdb}`
+        );
       });
     }
-  }  
-    ou */
-    public static listarFilmes(): void {
-      if (Filme.filmes.length === 0) {
-        console.log("Nenhum filme cadastrado.");
-      } else {
-        console.log("Listando filmes:");
-        Filme.filmes.forEach(filme => {
-          console.log(`Título: ${filme.titulo}, Autor: ${filme._autor}, Ano: ${filme._ano}, País: ${filme._pais}, IMDb: ${filme._imdb}`);
-        });
-      }
-      // TODO: salvar a lista no CSV
-    }
+    // TODO: salvar a lista no CSV
+  }
 }
