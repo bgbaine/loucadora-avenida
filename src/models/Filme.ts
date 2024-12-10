6957c42b5b52a0cad53bb19dfd0f548b1046c494
BryanLaquimam <bryanlaquimam256@gmail.com>
2024-12-09 21:37:29 -0300

    Filme

--- src/models/Filme.ts
+++ src/models/Filme.ts
@@ -32,19 +32,65 @@ export class Filme {
     se for caso: passar para csv ANTES 
     public adicionarFilme(): void  {}*/
 
+    public static adicionarFilme(filme: Filme): void {
+      Filme.filmes.push(filme);
+      console.log(`Filme ${filme.titulo} adicionado com sucesso.`);
+    }
+
   // TODO: implementar metodo atualizarFilme(): passar para csv ANTES
-  public atualizarFilme(): void {
-    console.log(`atualizando filme: ${this.titulo}`);
+  public atualizarFilme(titulo?: string, autor?: string, ano?: number, pais?: string): void {
+    if (titulo) this._titulo = titulo;
+    if (autor) this._autor = autor;
+    if (ano) this._ano = ano;
+    if (pais) this._pais = pais;
+  
+    console.log(`Filme ${this.titulo} atualizado com sucesso.`);
+    // TODO: salvar alterações no CSV
   }
 
   // TODO: implementar metodo removerFilme(): passar para csv ANTES
-  public removerFilme(): void {
-    console.log(`removendo filme: ${this.titulo}`);
+  public static removerFilme(imdb: string): void {
+    const index = Filme.filmes.findIndex(filme => filme._imdb === imdb);
+    if (index !== -1) {
+      const filmeRemovido = Filme.filmes.splice(index, 1);
+      console.log(`Filme ${filmeRemovido[0].titulo} removido com sucesso.`);
+    } else {
+      console.log("Filme não encontrado.");
+    }
+    // TODO: atualizar o CSV após remoção
   }
 
   // TODO: passar para csv
-  public static listarFilmes(): Filme[] {
+ /*  public static listarFilmes(): Filme[] {
     console.log("Listando filmes...");
     return Filme.filmes;
-  }
+  } */
+
+  /* public static listarFilmes(): void {
+    if (Filme.filmes.length === 0) {
+      console.log("Nenhum filme cadastrado.");
+    } else {
+      console.log("Listando filmes:");
+      Filme.filmes.forEach(filme => {
+        console.log(`Título: ${filme.titulo}`);
+        console.log(`Autor: ${filme._autor}`);
+        console.log(`Ano: ${filme._ano}`);
+        console.log(`País: ${filme._pais}`);
+        console.log(`IMDB: ${filme._imdb}`);
+        console.log("----");
+      });
+    }
+  }  
+    ou */
+    public static listarFilmes(): void {
+      if (Filme.filmes.length === 0) {
+        console.log("Nenhum filme cadastrado.");
+      } else {
+        console.log("Listando filmes:");
+        Filme.filmes.forEach(filme => {
+          console.log(`Título: ${filme.titulo}, Autor: ${filme._autor}, Ano: ${filme._ano}, País: ${filme._pais}, IMDb: ${filme._imdb}`);
+        });
+      }
+      // TODO: salvar a lista no CSV
+    }
 }
