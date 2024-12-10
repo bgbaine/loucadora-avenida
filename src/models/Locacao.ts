1c5c56d361b4ef427a0b6b645774d38cbd476832
Bernardo <bgbaine@gmail.com>
2024-12-09 22:59:49 -0300

    feat: implement Locacao class' methods

--- src/models/Locacao.ts
+++ src/models/Locacao.ts
@@ -2,49 +2,77 @@ import { Cliente } from "./Cliente";
 import { Filme } from "./Filme";
 
 export class Locacao {
+  private _id: number;
   private _cliente: Cliente;
   private _filme: Filme;
 
   /* Bem possivel que utilizar esses campos com tipo Date quebre tudo
   fazer o que, ne ^^ */
-  private _dataLocacao: Date;
-  private _dataEntrega: Date;
+  private _dataLocacao: number;
+  private _dataEntrega: number | undefined;
 
   // TODO: passar para csv
   private static locacoesAtivas: Locacao[] = [];
   private static locacoes: Locacao[] = [];
 
   // TODO: passar para csv
-  constructor(
-    cliente: Cliente,
-    filme: Filme,
-    dataLocacao: Date,
-    dataEntrega: Date
-  ) {
+  constructor(cliente: Cliente, filme: Filme, dataLocacao: number) {
+    this._id = Math.floor(Math.random() * 1_000);
     this._cliente = cliente;
     this._filme = filme;
     this._dataLocacao = dataLocacao;
-    this._dataEntrega = dataEntrega;
-    Locacao.locacoes.push(this);
-    Locacao.locacoesAtivas.push(this);
+  }
+
+  public get id(): number {
+    return this._id;
   }
 
   /* TODO: dependendo da arquitetura, criar metodo realizarLocacao()
-    se for o caso: passar para csv ANTES
-    public realizarLocacao(): void  {}*/
+    se for o caso: passar para csv ANTES*/
+  public static realizarLocacao(locacao: Locacao): void {
+    Locacao.locacoes.push(locacao);
+    Locacao.locacoesAtivas.push(locacao);
+    console.log(`Locacao ${locacao.id} cadastrada com sucesso.`);
+  }
 
   // TODO: implementar metodo encerrarLocacao(): passar para csv ANTES
-  public encerrarLocacao(): void {
-    console.log("encerrando locacao");
+  public static encerrarLocacao(id: number): void {
+    const index = Locacao.locacoesAtivas.findIndex(
+      (locacao) => locacao._id === id
+    );
+    if (index !== -1) {
+      const locacaoRemovida = Locacao.locacoesAtivas.splice(index, 1);
+      console.log(`Locacao #${locacaoRemovida[0].id} removida com sucesso.`);
+    } else {
+      console.log("Locacao não encontrada.");
+    }
   }
 
   // TODO: implementar metodo listarLocacoes(): passar para csv ANTES
-  public static listarLocacoes(): Locacao[] {
-    return Locacao.locacoesAtivas;
+  public static listarLocacoes(): void {
+    if (Locacao.locacoesAtivas.length === 0) {
+      console.log("Nenhuma locacao ativa.");
+    } else {
+      console.log("Listando locacoes ativas:");
+      Locacao.locacoes.forEach((locacao) => {
+        console.log(
+          `ID: ${locacao.id}, Cliente: ${locacao._cliente.nome}, Filme: ${locacao._filme.titulo}, Data da Locacao: ${locacao._dataLocacao}`
+        );
+      });
+    }
   }
 
   // TODO: passar para csv
-  public static listarHistorico(): Locacao[] {
-    return Locacao.locacoes;
+  public static listarHistorico(): void {
+    if (Locacao.locacoesAtivas.length === 0) {
+      console.log("Nenhuma locacao ativa.");
+    } else {
+      console.log("Listando locacoes ativas:");
+      Locacao.locacoes.forEach((locacao) => {
+        console.log(
+          `ID: ${locacao.id}, Cliente: ${locacao._cliente.nome}, Filme: ${locacao._filme.titulo}, Data da Locacao: ${locacao._dataLocacao}, Data da Entrega: ${locacao._dataEntrega}`
+        );
+      });
+    }
   }
 }
